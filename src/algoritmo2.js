4afa62b9e58b35f9edbb21f7f474d7ae2f5a056a
Guilherme Moraes <127672759+guilhermemoraes1@users.noreply.github.com>
2024-11-13 14:58:30 -0300

    Update algoritmo2.js

--- src/algoritmo2.js
+++ src/algoritmo2.js
@@ -5,12 +5,18 @@ const letras = [
     ['d', 'f', 'g'],
 ];
    
-// 
+// usa o flat pra deixar todos os elementos dentro de um mesmo array
 let resultado = letras.flat().reduce((objeto, numero) => {
+    // usa o reduce pra contar o número de elementos repetido
     if (objeto[numero]) {
+        // se o objeto já foi iterado antes adiciona mais 1 no valor da chave
         objeto[numero] += 1;
+        
     } else {
+        // se o objeto ainda não foi iterado adicionar o valor 1 na chave atual
         objeto[numero] = 1;
     }
+
+    // retorna objeto atualizado
     return objeto;
-}, {});
+}, {}); // inicializa o acumulador com um objeto vazio
