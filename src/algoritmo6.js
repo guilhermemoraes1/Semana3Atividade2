888551487cc913937545e676606d5cf80a39cb43
Guilherme Moraes <127672759+guilhermemoraes1@users.noreply.github.com>
2024-11-13 14:34:56 -0300

    Update algoritmo6.js

--- src/algoritmo6.js
+++ src/algoritmo6.js
@@ -1,3 +1,5 @@
+// Construa uma string que representa uma estrutura em HTML que seja capaz de listar de forma ordenada os itens = ["Guarabira", "Belém", "Sapé"]
+
 const itens = ["Guarabira", "Belém", "Sapé"];
 
 function criarElemenentoLi() {
